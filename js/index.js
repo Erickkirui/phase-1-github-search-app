8771fecc56a33099a842a1444129b6c8baaa7094
Erickkirui <erickkirui653@gmail.com>
2023-04-03 15:01:55 +0300

    fetch user from gihub api and display name from the form

--- js/index.js
+++ js/index.js
@@ -4,25 +4,27 @@ document.addEventListener('DOMContentLoaded', () => {
     function handleInputOfName(){
         let userInput = document.querySelector("#github-form")
         //event listener
-        userInput.addEventListener('submit',(e) => {
+         userInput.addEventListener('submit',(e) => {
             e.preventDefault()
-            console.log(e.target.search.value)
+            fetchTheUsersFromSearch(e.target.search.value)
         })
        
        }
-       
        handleInputOfName()
-  });
 
-
-
-
-
-
-
-
-
-
-
+       //Fetch endponts for the usernames 
+       function fetchTheUsersFromSearch(UseName){
+        fetch(`https://api.github.com/search/users?q=${UseName}`)
+        .then(response => response.json())
+        .then( data => {
+          const userList =document.querySelector("#user-list")
+          userList.textContent =""
+          data.items.forEach(item => {
+            const li = document.createElement("li");
+            li.textContent = item.login;
+            userList.appendChild(li);
+       })
+      })      
+    }})
 
 
